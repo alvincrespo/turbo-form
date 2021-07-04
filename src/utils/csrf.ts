6af623af38ecec547c70b4164c35a95eeb82b521
Alvin Crespo <alvin.crespo@gmail.com>
2021-07-03 20:16:00 -0400

    add ref comment for csrf

--- src/utils/csrf.ts
+++ src/utils/csrf.ts
@@ -3,6 +3,7 @@ interface CSRFElements {
   token: HTMLMetaElement | null
 }
 
+// Reference: https://github.com/rails/rails/commit/ad3a47759e67a411f3534309cdd704f12f6930a7#diff-86b08dbffd1c08433d06cd4f33e9e4bc2b2a1ccf14d466a369713d98d48b3ea8
 export class Csrf {
   private _elements: CSRFElements = {
     param: document.querySelector('meta[name=csrf-param]'),
